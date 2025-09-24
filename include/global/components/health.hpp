ef9d0da6819eda4549b4d84af43518f4e0259104
Yuuko <tilio.huart@epitech.eu>
2025-09-24 08:48:32 +0200

    feat: add Health component and basic HealthSystem

---
+++ include/global/components/health.hpp
@@ -0,0 +1,18 @@
+//
+// EPITECH PROJECT, 2025
+// r-type
+// File description:
+// health component
+//
+
+
+#pragma once
+
+#include <cstdint>
+
+struct Health
+{
+    uint8_t healthPoint = 100;
+
+    Health() = default;
+};
---
+++ systems/healthSystem.cpp
@@ -0,0 +1,21 @@
+//
+// EPITECH PROJECT, 2025
+// r-type
+// File description:
+// healthSystem
+//
+
+#include "flux/core/flux.hpp"
+#include "health.hpp"
+
+void HealthSystem(flux::ECS ecs, flux::Entity e1)
+{
+    if (!ecs.HasComponent<Health>(e1))
+        return;
+    auto& [healthPoint] = ecs.GetComponent<Health>(e1);
+    if (healthPoint >= 10) {
+        healthPoint -= 10;
+    } else {
+        healthPoint = 0;
+    }
+}
