b20e935ddad89e831b7243e11288141556d2610a
Yuuko <146713844+TilioHuart@users.noreply.github.com>
2025-09-25 14:45:49 +0200

    feat: add basic playerInput component (#14)
    * feat: add basic playerInput component
    
    * fix: remove speed from playerInput

---
+++ include/global/components/playerInput.hpp
@@ -0,0 +1,18 @@
+//
+// EPITECH PROJECT, 2025
+// r-type
+// File description:
+// movement
+//
+
+namespace component
+{
+    struct PlayerInput
+    {
+            bool move_right = false;
+            bool move_left = false;
+            bool move_up = false;
+            bool move_down = false;
+            bool shoot = false;
+    };
+} // namespace component
