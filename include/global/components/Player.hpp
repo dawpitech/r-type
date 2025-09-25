a2be272bfad592baaff8f034953f097df328be12
Yuuko <tilio.huart@epitech.eu>
2025-09-25 09:27:21 +0200

    feat: add basic player component

---
+++ include/global/components/Player.hpp
@@ -0,0 +1,17 @@
+//
+// EPITECH PROJECT, 2025
+// r-type
+// File description:
+// player
+//
+
+namespace component
+{
+    struct player
+    {
+            int score = 0;
+            int nbLive = 3;
+            int lvl = 0;
+    };
+
+} // namespace component
