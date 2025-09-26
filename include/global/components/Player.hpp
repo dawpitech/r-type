0567cfb038f5e666349e5463981c3069d7f55f05
Yuuko <146713844+TilioHuart@users.noreply.github.com>
2025-09-26 08:59:37 +0200

    feat: add basic player component (#15)
    * feat: add basic player component
    
    * chore: remove the number of lives from the player component

---
+++ include/global/components/Player.hpp
@@ -0,0 +1,16 @@
+//
+// EPITECH PROJECT, 2025
+// r-type
+// File description:
+// player
+//
+
+namespace component
+{
+    struct player
+    {
+            int score = 0;
+            int lvl = 0;
+    };
+
+} // namespace component
