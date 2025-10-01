f8b5972ff5fc85e386beabd2ae913144edc24265
Valiance <lucas.ludovic@epitech.eu>
2025-10-01 14:25:01 +0200

    change: use logger instead of cout when adding player

--- server/src/Player/PlayersManager.cpp
+++ server/src/Player/PlayersManager.cpp
@@ -7,10 +7,10 @@
 
 #include "Player/PlayersManager.hpp"
 #include <iostream>
-#include "Network/TCP/TCPInfo.hpp"
+#include "utils/logger.hpp"
 
 void game::PlayersManager::createNewPlayer(const network::ConnectionInfo &info)
 {
-    std::cout << "New player added with uuid: " << info.uuid << std::endl;
+    utils::Logger::debug(std::format("New player added with uuid: {}", info.uuid));
     this->_players.emplace_back(std::make_unique<Player>(info));
 }
