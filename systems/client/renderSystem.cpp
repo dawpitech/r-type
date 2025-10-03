c97f4a96d462c9463b5cd75ca482e0901aebca73
Yuuko <tilio.huart@epitech.eu>
2025-10-03 16:31:15 +0200

    refacto: remove debug

--- systems/client/renderSystem.cpp
+++ systems/client/renderSystem.cpp
@@ -5,7 +5,6 @@
 // healthSystem
 //
 
-#include <iostream>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3_image/SDL_image.h>
@@ -14,7 +13,6 @@
 #include "client/components/sprite.hpp"
 #include "flux/core/flux.hpp"
 #include "client/sdlManager.hpp"
-#include "global/components/playerInput.hpp"
 
 void RenderSystem(flux::ECS& ecs, flux::Entity entity)
 {
@@ -23,22 +21,9 @@ void RenderSystem(flux::ECS& ecs, flux::Entity entity)
 
     auto& sprite = ecs.GetComponent<component::sprite>(entity);
     auto& animation = ecs.GetComponent<component::animation>(entity);
-    auto &playerInput = ecs.GetComponent<component::PlayerInput>(entity);
 
     if (!sprite.visible)
         return;
 
-    if (playerInput.move_down) {
-        sprite.destRect.y += 0.01;
-    }
-    if (playerInput.move_up) {
-        sprite.destRect.y -= 0.01;
-    }
-    if (playerInput.move_right) {
-        sprite.destRect.x += 0.01;
-    }
-    if (playerInput.move_left) {
-        sprite.destRect.x -= 0.01;
-    }
     render::SDLManager::renderTexture(sprite.texture, sprite.srcRect, sprite.destRect);
 }
