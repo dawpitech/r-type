a9d987ef78ba1386fe8c05bd1be1ccbab3785595
ALittlePatate <48603993+ALittlePatate@users.noreply.github.com>
2025-10-06 12:28:19 +0200

    feat: add arguments parsing for the client (#42)

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,15 +5,51 @@
 ** main.cpp
 */
 
+#include <boost/program_options/errors.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+#include <format>
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include "client/game.hpp"
 #include "global/utils/error.hpp"
+#include "global/utils/logger.hpp"
+#include "client/parseArgs.hpp"
 
-int main()
+static void checkVariables(po::variables_map& variables)
+{
+    if (variables.count("ip") != 1)
+    {
+        throw utils::ParsingError("Arg ip undefined", "checkVariables");
+    }
+    if (variables.count("port") != 1)
+    {
+        throw utils::ParsingError("Arg port undefined", "checkVariables");
+    }
+    if (variables.find("debug") != variables.end())
+    {
+        utils::Logger::setDebug(true);
+    }
+}
+
+int main(int argc, char **argv)
 {
     try {
         rTypeClient::Game game;
+        utils::Parser parser(argc, argv);
+        parser.parseArgs();
+        po::variables_map variables = parser.getArgs();
+
+        checkVariables(variables);
+        std::string msg =
+            std::format("IP: {}", variables["ip"].as<std::string>());
+        utils::Logger::debug(msg);
+        msg = std::format("Port: {}", variables["port"].as<uint16_t>());
+        utils::Logger::debug(msg);
+
+        std::string ip = variables["ip"].as<std::string>();
+        std::uint16_t port = variables["port"].as<uint16_t>();
         game.launchGame();
     }
     catch (const rTypeClient::GameError& e) {
@@ -21,6 +57,11 @@ int main()
     }
     catch (const utils::BaseError& e) {
         std::cerr << e.what() << "in" << e.where() << std::endl;
+    }
+    catch (const po::error& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
---
+++ include/client/parseArgs.hpp
@@ -0,0 +1,54 @@
+//
+// EPITECH PROJECT, 2025
+// R-type
+// File description:
+// Parse args from command line
+//
+#pragma once
+
+#include <boost/program_options.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include "global/utils/error.hpp"
+
+namespace po = boost::program_options;
+
+namespace utils
+{
+    class ParsingError final : public BaseError
+    {
+        public:
+            ParsingError(const std::string& msg, const std::string& where) :
+                BaseError(msg, where) {};
+    };
+
+    class Parser
+    {
+        public:
+            explicit Parser(int argc, char** argv) :
+                _desc("Options"), _argc(argc), _argv(argv)
+            {
+                this->_desc.add_options()("help,h", "Display help message")(
+                    "debug,d", "Enable debug mode")(
+                    "port,p", po::value<uint16_t>(),
+                    "Set port to connect to")("ip,i", po::value<std::string>(),
+                                          "Ip of the server");
+            };
+            ~Parser() = default;
+
+            void parseArgs()
+            {
+                po::store(po::parse_command_line(this->_argc, this->_argv,
+                                                 this->_desc),
+                          this->_variables);
+                po::notify(this->_variables);
+            };
+
+            po::variables_map getArgs() { return this->_variables; }
+
+        private:
+            int _argc;
+            char** _argv;
+            po::options_description _desc;
+            po::variables_map _variables;
+    };
+} // namespace utils
