42cab7cbab098a1620451612e5a4faedef5e2b5c
Yuuko <tilio.huart@epitech.eu>
2025-10-09 10:06:25 +0200

    fead: collider on player projectile

--- common/src/systems/shootSystem.cpp
+++ common/src/systems/shootSystem.cpp
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include "components/Animation.hpp"
+#include "components/Collider.hpp"
+#include "components/Health.hpp"
 #include "components/Player.hpp"
 #include "components/PlayerInput.hpp"
 #include "components/Projectile.hpp"
@@ -30,6 +32,7 @@ void ShootSystem(flux::ECS& ecs, const std::vector<flux::Entity>& entities)
             auto& playerTransform = ecs.GetComponent<component::Transform>(entity);
             flux::Entity projectile = ecs.newEntity();
 
+            ecs.Add<component::Projectile>(projectile);
             ecs.Add<component::sprite>(
                 projectile,
                 component::sprite(render::SDLManager::load("./assets/playerProjectile-mod-resize.gif").texture));
@@ -45,7 +48,13 @@ void ShootSystem(flux::ECS& ecs, const std::vector<flux::Entity>& entities)
                     playerTransform.pos.x + render::SDLManager::load("./assets/player.gif").frameSize.x + 10,
                     playerTransform.pos.y + render::SDLManager::load("./assets/player.gif").frameSize.y / 2, 0, 1, 1));
             ecs.Add<component::Velocity>(projectile, component::Velocity(1, 0));
-            ecs.Add<component::Projectile>(projectile);
+            ecs.Add<component::collider>(
+                projectile,
+                component::collider(
+                    component::PLAYER_PROJECTILE, component::MOB,
+                    {0, 0, render::SDLManager::load("./assets/playerProjectile-mod-resize.gif").frameSize.x,
+                     render::SDLManager::load("./assets/playerProjectile-mod-resize.gif").frameSize.y}));
+            ecs.Add<component::Health>(projectile);
 
             input.shoot = false;
         }
