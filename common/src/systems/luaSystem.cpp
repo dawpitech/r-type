b0c6830ea95c0111999f7fd625031043b18a377d
Antoine <me@antoinebellanger.fr>
2025-10-21 23:22:41 +0200

    feat: lua can access Transform and Velocity components content

--- common/src/systems/luaSystem.cpp
+++ common/src/systems/luaSystem.cpp
@@ -9,6 +9,7 @@
 #include "LuaContext.hpp"
 
 #include <components/Transform.hpp>
+#include <components/Velocity.hpp>
 
 flux::View LuaSystemView(const flux::ECS &ecs) {
     return flux::View{};
@@ -17,10 +18,31 @@ flux::View LuaSystemView(const flux::ECS &ecs) {
 void LuaSystem(flux::ECS& ecs, const std::vector<flux::Entity>& entities) {
     std::cout << "TRACE: C++: LUA SYSTEM TRIGGERED" << std::endl;
 
+    const auto lua = LuaContextStore::getInstance().getLuaContext();
     const auto& luaSystems = LuaContextStore::getInstance().getLuaSystems();
     for (const auto entity : entities) {
         for (const auto& system : luaSystems) {
-            
+            sol::table entityComponents = lua->create_table();
+            int entityComponentsSize = 0;
+
+            for (const auto& componentRequirement : system.componentsRequired) {
+                //std::cout << "Searching for component " << componentRequirement << std::endl;
+                if (componentRequirement == "Transform" && ecs.HasComponent<component::Transform>(entity)) {
+                    entityComponents["transform"] = ecs.GetComponent<component::Transform>(entity);
+                    entityComponentsSize++;
+                } else if (componentRequirement == "Velocity" && ecs.HasComponent<component::Velocity>(entity)) {
+                    entityComponents["velocity"] = ecs.GetComponent<component::Velocity>(entity);
+                    entityComponentsSize++;
+                }
+            }
+            if (system.componentsRequired.size() != entityComponentsSize) {
+                //std::cout << "Crapping out: " << system.componentsRequired.size() << "/" << entityComponentsSize++; << std::endl;
+                continue;
+            }
+            if (const auto result = system.fn(entityComponents); !result.valid()) {
+                sol::error err = result;
+                std::cerr << "ERROR: C++: Error while running lua external code " << err.what() << std::endl;
+            }
         }
     }
 }
--- scripts/mob1.lua
+++ scripts/mob1.lua
@@ -1,22 +1,16 @@
 print("TRACE: Lua: Loading enemy behavior script.")
 
----[[
--- Define the system logic
 local function enemy_ai_system(entity)
+    print("Processing an entity:")
     -- Access components passed from C++
     local transform = entity.transform
     local velocity = entity.velocity
 
-    print("TRACE: Lua: Updating entity. Transform is at x=" .. transform.x)
-
-    -- Implement behavior
-    if transform.x < 100 then
-        velocity.dx = 5
-    else
-        velocity.dx = -5
-    end
+    print("\tpos x=" .. transform.pos.x)
+    print("\tpos y=" .. transform.pos.y)
+    print("\tvelocity x=" .. velocity.x)
+    print("\tvelocity y=" .. velocity.y)
 end
 
 -- Register the system with its component dependencies
 ecs.register_system(enemy_ai_system, { "Transform", "Velocity" })
---]]
