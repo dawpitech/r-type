865044707bf75fc16e0ae0978a9b5c7b66f5fe2a
Valiance <146172201+LucasLudovic@users.noreply.github.com>
2025-10-21 13:52:01 +0200

    refacto: move client logic from main to a handler class (#84)
    * feat: add raylib and raylib-cpp libs
    
    * refacto: move to raylib part I
    
    * fix: update CI required packages from SDL3 needs to raylib ones
    
    * fix: remove old include, remove unused libs fix shutting down the window
    
    * refacto: part II -> server
    
    * refacto: part III animation system
    
    * fix: register system in network
    
    * fix: wait until room is ready
    
    * Fix/rooms (#70)
    
    * fix: correct crash when multiple rooms
    
    * fix: extand scope of timer
    
    * chore: add rule for just with libasan
    
    * change: remove chrono for server game tick
    
    the chrono is still used to send data
    
    * refacto: use template class to store the serialized data
    
    * refacto: init hooks in its own method and rename Roomspool to server
    
    * fix: wait until everyone sent udp port before sending data through udp
    
    * fix: run tcp channel each turn
    
    * refacto: remove useless method
    
    * refacto: serialize component in single function
    
    * feat: add data compression to reduce network overload
    
    * feat: store game state for 15 previous ticks
    
    * refacto: add a new file for init
    
    * refacto: add class to handle client
    
    ---------
    
    Co-authored-by: Antoine <me@antoinebellanger.fr>

---
+++ client/src/Client.hpp
@@ -0,0 +1,53 @@
+//
+// EPITECH PROJECT, 2025
+// R-type
+// File description:
+// Main class to run client
+//
+
+#include "flux/core/flux.hpp"
+#include "network/TCPClient.hpp"
+#include "network/UDPClient.hpp"
+#include "network/datatype.hpp"
+#include <Window.hpp>
+#include <chrono>
+#include <memory>
+#include <queue>
+
+namespace client {
+
+    constexpr unsigned WINDOW_BASE_WIDTH = 800;
+    constexpr unsigned WINDOW_BASE_HEIGHT = 450;
+    constexpr std::string WINDOW_BASE_NAME = "R-Type";
+    constexpr unsigned INPUT_SEND_DELAY = 20;
+
+    class Client {
+       public:
+        Client(const std::string &ip, uint16_t port);
+
+        void run();
+
+       private:
+        flux::ECS _ecs;
+        flux::runtimeHooks _hooks;
+
+        std::string _ip;
+        unsigned _port = 0;
+        unsigned _inputIndex = 0;
+
+        std::unique_ptr<raylib::Window> _window = nullptr;
+        std::unique_ptr<client::network::TCPClient> _networkTCPClient = nullptr;
+        std::unique_ptr<client::network::UDPClient> _networkUDPClient = nullptr;
+
+        std::queue<component::PlayerInput> _inputs;
+        ::network::ClientTCPSentInfo _gameInfo;
+        std::chrono::steady_clock::time_point _lastInputSend;
+        component::PlayerInput _lastSentInput;
+
+        void _initHooks();
+        void _initNetworkableHooks();
+        void _registerBase();
+        void _sendPlayerInput(const std::unordered_map<flux::Entity, std::vector<std::any>> &componentStore,
+            const std::chrono::steady_clock::time_point &now);
+    };
+}  // namespace client
---
+++ server/src/rooms/Base.cpp
@@ -0,0 +1,120 @@
+//
+// EPITECH PROJECT, 2025
+// R-type
+// File description:
+// Logic for each rooms
+//
+
+#include <format>
+#include <mutex>
+
+#include "Simulation.hpp"
+#include "components/NetworkIdentification.hpp"
+#include "components/Player.hpp"
+#include "flux/core/Serialization.hpp"
+#include "flux/core/flux.hpp"
+#include "player/Player.hpp"
+#include "rooms/Rooms.hpp"
+#include "utils/logger.hpp"
+#include <condition_variable>
+
+using std::uint8_t;
+
+Room::Room::Room(const std::size_t roomNumber, const std::uint8_t nbPlayers)
+    : _roomNumber(roomNumber), _nbPlayerMax(nbPlayers)
+{}
+
+void Room::Room::run()
+{
+    {
+        std::lock_guard<std::mutex> lock(this->_roomMutex);
+        Simulation::setInitialSimState(this->_ecs);
+    }
+    this->_setRoomReady();
+    this->_waitRoomFull();
+
+    flux::runtimeHooks hooks;
+    this->_initHooks(hooks);
+    this->_ecs.handExecution(hooks);
+}
+
+void Room::Room::_waitRoomFull()
+{
+    std::unique_lock<std::mutex> lock(this->_roomMutex);
+    this->_fullCondition.wait(lock, [this] { return this->_players.size() >= BASEROOMPLAYER; });
+}
+
+void Room::Room::clear(const std::uint8_t nbPlayers)
+{
+    std::lock_guard<std::mutex> lock(this->_roomMutex);
+    this->_players.erase(this->_players.begin(), this->_players.end());
+    this->_nbPlayerMax = nbPlayers;
+}
+
+bool Room::Room::addPlayer(game::Player &player)
+{
+    std::lock_guard<std::mutex> lock(this->_roomMutex);
+    if (this->_players.size() >= this->_nbPlayerMax) {
+        auto log = std::format("Too many player ! Can't add user in room {}", this->_roomNumber);
+        utils::Logger::debug(log);
+        return false;
+    }
+
+    this->_players.push_back(player);
+    player.assignRoom(this->_roomNumber);
+
+    this->_assignPlayerToEntity(player);
+    return true;
+}
+
+void Room::Room::notifyRoomFull()
+{
+    if (this->_players.size() >= BASEROOMPLAYER)
+        this->_fullCondition.notify_all();
+}
+
+bool Room::Room::isRoomFull()
+{
+    std::lock_guard<std::mutex> lock(this->_roomMutex);
+    return this->_players.size() >= this->_nbPlayerMax;
+}
+
+void Room::Room::_setRoomReady()
+{
+    std::lock_guard<std::mutex> lock(this->_readyMutex);
+    this->_isReady = true;
+    this->_readyCondition.notify_all();
+}
+
+void Room::Room::waitRoomReady()
+{
+    std::unique_lock<std::mutex> lock(this->_readyMutex);
+    this->_readyCondition.wait(lock, [this] { return this->_isReady; });
+}
+
+void Room::Room::_assignPlayerToEntity(game::Player &player)
+{
+    const auto playerView = this->_ecs.GenerateViewFromComponents<component::Player>();
+    const auto allPlayerEntities = this->_ecs.QueryViewNotExclusive(playerView);
+
+    for (auto entity : allPlayerEntities) {
+        if (this->_ecs.HasComponent<component::NetworkIdentification>(entity)) {
+            auto &existingId = this->_ecs.GetComponent<component::NetworkIdentification>(entity);
+            if (std::strcmp(existingId.uuid, player.getId().c_str()) == 0) {
+                return;
+            }
+        }
+    }
+
+    for (auto entity : allPlayerEntities) {
+        if (!this->_ecs.HasComponent<component::NetworkIdentification>(entity)) {
+            component::NetworkIdentification id{};
+            std::strcpy(id.uuid, player.getId().c_str());
+            this->_ecs.AddOrReplace(entity, id);
+            player.assignEntity(entity);
+            utils::Logger::debug(
+                std::format("Assigned uuid {} to entity {}", player.getId(), player.getEntity()));
+            return;
+        }
+    }
+}
