8143dfcc6731aab0c321afbc0b058f69fa6b480c
Valiance <146172201+LucasLudovic@users.noreply.github.com>
2025-10-29 13:37:29 +0100

    feat:cli add admin cli commands (#103)
    * feat: add basic admin command like exit
    
    * feat: add admin command to display rooms infos
    
    * feat: get info for single room
    
    * feat: admin is able to reset the server rooms
    
    * feat: admin is able to reset a single room
    
    * merge: change simstate to serverSimState to load game

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -6,39 +6,39 @@
 //
 
 #include <chrono>
+#include <exception>
 #include <format>
 #include <optional>
+#include <string>
 #include <thread>
 
 #include "Server.hpp"
-#include "rooms/Rooms.hpp"
+#include "admin/AdminHandler.hpp"
 #include "network/UDP/UDPNetwork.hpp"
 #include "network/datatype.hpp"
+#include "rooms/Rooms.hpp"
 #include "utils/logger.hpp"
 
-Server::Server::Server(std::uint16_t port, std::uint16_t nbRooms)
-    : _nbRooms(nbRooms), _connectionNetwork(port), _gameUpdateNetwork(port)
+Server::Server::Server(std::uint16_t port, std::uint16_t nbRooms, bool cli)
+    : _nbRooms(nbRooms), _connectionNetwork(port), _gameUpdateNetwork(port), _cli(cli)
 {
-    this->_connectionNetwork.attach<network::ConnectionInfo>(
-        [this](const network::ConnectionInfo &info) {
-            this->_playerManager.createNewPlayer(
-                info, this->_gameUpdateNetwork);
-            auto playerOpt = this->_playerManager.getPlayer(info.uuid);
-            if (!playerOpt.has_value()) {
-                return;
+    this->_connectionNetwork.attach<network::ConnectionInfo>([this](const network::ConnectionInfo &info) {
+        this->_playerManager.createNewPlayer(info, this->_gameUpdateNetwork);
+        auto playerOpt = this->_playerManager.getPlayer(info.uuid);
+        if (!playerOpt.has_value()) {
+            return;
+        }
+        auto cpt = 0;
+        for (auto &room : this->_rooms) {
+            if (!room->isRoomFull()) {
+                room->waitRoomReady();
+                utils::Logger::debug(std::format("Player added to room {}", cpt));
+                room->addPlayer(playerOpt.value());
+                break;
             }
-            auto cpt = 0;
-            for (auto &room : this->_rooms) {
-                if (!room->isRoomFull()) {
-                    room->waitRoomReady();
-                    utils::Logger::debug(
-                        std::format("Player added to room {}", cpt));
-                    room->addPlayer(playerOpt.value());
-                    break;
-                }
-                cpt += 1;
-            };
-        });
+            cpt += 1;
+        };
+    });
     this->_connectionNetwork.attach<network::ClientTCPReceivedInfo>(
         [this](network::ClientTCPReceivedInfo info) {
             this->_playerManager.storeInfo(info);
@@ -52,11 +52,14 @@ Server::Server::Server(std::uint16_t port, std::uint16_t nbRooms)
             }
         });
     this->_gameUpdateNetwork.attach<network::UDPReceivedInfo>(
-        [this](network::UDPReceivedInfo info) {
-            this->_playerManager.storeInput(info);
-        });
+        [this](network::UDPReceivedInfo info) { this->_playerManager.storeInput(info); });
 
-    for (uint16_t i = 0; i < nbRooms; i += 1) {
+    this->_setupRooms();
+}
+
+void Server::Server::_setupRooms()
+{
+    for (uint16_t i = 0; i < this->_nbRooms; i += 1) {
         this->_rooms.push_back(std::make_unique<Room::Room>(i));
         Room::Room *roomPtr = this->_rooms.back().get();
         this->_threads.emplace_back([roomPtr] {
@@ -66,16 +69,79 @@ Server::Server::Server(std::uint16_t port, std::uint16_t nbRooms)
     }
 }
 
-Server::Server::~Server() {}
+Server::Server::~Server()
+{
+    for (auto &room : this->_rooms) {
+        room->stop();
+    }
+    for (auto &thread : this->_threads) {
+        thread.join();
+    }
+}
 
 void Server::Server::run()
 {
+    if (this->_cli) {
+        std::thread cliThread([this] {
+            AdminHandler admin(*this);
+            std::cout << "Welcome Dear Administrator" << std::endl;
+            while (this->_isRunning) {
+                std::string input;
+                std::cout << ">> ";
+                std::getline(std::cin, input);
+                std::cin.clear();
+                admin.executeInput(input);
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+        });
+        cliThread.detach();
+    }
     while (this->_isRunning) {
         this->_connectionNetwork.connect();
         this->_gameUpdateNetwork.connect();
         std::this_thread::sleep_for(std::chrono::nanoseconds(10));
     }
+}
+
+void Server::Server::displayRoomsInfos()
+{
+    for (auto &it : this->_rooms) {
+        it->displayInfo();
+    }
+}
+
+void Server::Server::displayRoomInfos(uint8_t roomNumber)
+{
+    if (roomNumber >= this->_rooms.size())
+        return;
+    this->_rooms[roomNumber]->displayInfo();
+}
+
+void Server::Server::resetRooms()
+{
+    for (auto &room : this->_rooms) {
+        room->stop();
+    }
     for (auto &thread : this->_threads) {
         thread.join();
     }
+    this->_rooms.clear();
+    this->_threads.clear();
+    this->_setupRooms();
+}
+
+void Server::Server::resetRoom(uint8_t roomNumber)
+{
+    if (roomNumber >= this->_rooms.size())
+        return;
+    this->_rooms[roomNumber]->stop();
+    std::cout << "Reset room number " << roomNumber << std::endl;
+    this->_rooms[roomNumber] = std::make_unique<Room::Room>(roomNumber);
+    Room::Room *roomPtr = this->_rooms[roomNumber].get();
+    std::cout << "Before thread: " << roomNumber << std::endl;
+    this->_threads[roomNumber] = std::jthread([roomPtr] {
+        if (roomPtr)
+            roomPtr->run();
+    });
+    std::cout << "After thread: " << roomNumber << std::endl;
 }
