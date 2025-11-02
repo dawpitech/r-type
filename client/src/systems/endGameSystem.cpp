6f2507bf559e206d5647437c854dc9cd97170ff7
Valiance <lucas.ludovic@epitech.eu>
2025-11-02 22:03:52 +0100

    feat: display alive players score

--- client/src/systems/endGameSystem.cpp
+++ client/src/systems/endGameSystem.cpp
@@ -12,42 +12,49 @@
 #include "flux/core/flux.hpp"
 
 #include <CameraRaylib.hpp>
-#include <components/Transform.hpp>
 #include <Functions.hpp>
+#include <components/Transform.hpp>
 #include <string>
 
-flux::View endGameSystemView(const flux::ECS& ecs)
+flux::View endGameSystemView(const flux::ECS &ecs)
 {
     return ecs.GenerateViewFromComponents<component::Camera, component::Transform, component::EndGame>();
 }
 
-void endGameSystem(flux::ECS& ecs, const std::vector<flux::Entity>& entities)
+void endGameSystem(flux::ECS &ecs, const std::vector<flux::Entity> &entities)
 {
     for (int i = static_cast<int>(entities.size()) - 1; i >= 0; --i) {
-        const flux::Entity& entity = entities[i];
+        const flux::Entity &entity = entities[i];
         auto transform = ecs.GetComponent<component::Transform>(entity);
         auto endGameComponent = ecs.GetComponent<component::EndGame>(entity);
 
         if (transform.pos.x >= endGameComponent.endX) {
-            std::string text = "End Game";
-            int fontSize = 20;
-            int textWidth = raylib::MeasureText(text, fontSize);
-            int centerX = transform.pos.x - textWidth / 2;
-            int centerY = transform.pos.y - fontSize / 2;
-            raylib::DrawText(text, centerX, centerY, fontSize, GREEN);
-        }
-        const auto playerView = ecs.GenerateViewFromComponents<component::Player, component::NetworkIdentification>();
-        const auto allPlayerEntities = ecs.QueryViewNotExclusive(playerView);
-        for (auto entity : allPlayerEntities) {
-            auto player = ecs.GetComponent<component::Player>(entity);
-            if (transform.pos.x >= endGameComponent.endX) {
-                std::string text = "Score: " + std::to_string(player.score);
-                int fontSize = 20;
-                int textWidth = raylib::MeasureText(text, fontSize);
-                int centerX = transform.pos.x - textWidth / 2;
-                int centerY = transform.pos.y + 20 - fontSize / 2;
-                raylib::DrawText(text, centerX, centerY, fontSize, GREEN);
+            std::string textToDisplay = "End Game\n";
+
+            const auto playerView = ecs.GenerateViewFromComponents<component::Player>();
+            const auto allPlayerEntities = ecs.QueryViewNotExclusive(playerView);
+
+            int playerIndex = 0;
+            for (auto playerEntity : allPlayerEntities) {
+                auto player = ecs.GetComponent<component::Player>(playerEntity);
+
+                bool isLocalPlayer = ecs.HasComponent<component::NetworkIdentification>(playerEntity);
+
+                std::string scoreText =
+                    isLocalPlayer ? "You: " : "Player " + std::to_string(playerIndex + 1) + ": ";
+                scoreText += std::to_string(player.score);
+                scoreText += "\n";
+
+                playerIndex += 1;
+                textToDisplay += scoreText;
             }
+            int scoreFontSize = 18;
+            int scoreTextWidth = raylib::MeasureText(textToDisplay, scoreFontSize);
+            int scoreCenterX = transform.pos.x - scoreTextWidth / 2;
+            int scoreCenterY = transform.pos.y + 30 + (playerIndex * 25);
+
+            Color textColor = GREEN;
+            raylib::DrawText(textToDisplay, scoreCenterX, scoreCenterY, scoreFontSize, textColor);
         }
     }
 }
